96afc07813b7695457f3ba919188e005afbb30d0
Chris Meyer <cmeyer1969@gmail.com>
2017-08-03 09:39:18 -0700

    Fix remove background script; add align ZLP menu item.

--- nionswift_plugin/nion_hyperspy/HyperSpyUI.py
+++ nionswift_plugin/nion_hyperspy/HyperSpyUI.py
@@ -2,7 +2,7 @@
 import gettext
 
 # third part imports
-# None
+import nion.hyperspy
 
 # local libraries
 from nion.swift.model import DataItem
@@ -23,12 +23,16 @@ signal = nion.hyperspy.xdata_to_signal(src.display_xdata)
 fit_px = int(fit_region.interval[0] * src.display_xdata.data_shape[0]), int(fit_region.interval[1] * src.display_xdata.data_shape[0])
 signal_px = int(signal_region.interval[0] * src.display_xdata.data_shape[0]), int(signal_region.interval[1] * src.display_xdata.data_shape[0])
 signal = signal.remove_background(signal_range=fit_px)
-target.xdata = nion.hyperspy.signal_to_xdata(signal)[signal_px]
+target.xdata = nion.hyperspy.signal_to_xdata(signal)[signal_px[0]:signal_px[1]]
 ''',
-          'sources': [{'label': 'Source', 'name': 'src', 'regions': [
-              {'name': 'fit_region', 'params': {'label': 'Fit', 'interval': (0.2, 0.3)}, 'type': 'interval'},
-              {'name': 'signal_region', 'params': {'label': 'Signal', 'interval': (0.4, 0.5)}, 'type': 'interval'},
-          ]}],
+          'sources': [{'label': 'Source', 'name': 'src',
+                       'requirements': [{"type": "dimensionality", "min": 1, "max": 1}],
+                       'regions': [
+                           {'name': 'fit_region', 'params': {'label': 'Fit', 'interval': (0.2, 0.3)},
+                            'type': 'interval'},
+                           {'name': 'signal_region', 'params': {'label': 'Signal', 'interval': (0.4, 0.5)},
+                            'type': 'interval'},
+                       ]}],
           'title': 'Background Removed (HS)',
         }
 }
@@ -46,14 +50,18 @@ target.xdata = nion.hyperspy.signal_to_xdata(signal)[signal_px]
 DocumentModel.DocumentModel.register_processing_descriptions(processing_descriptions)
 
 
-class RemoveBackgroundMenuItem:
+class HyperSpyMenuItemBase:
 
     menu_id = "hyperspy_menu"  # required, specify menu_id where this item will go
     menu_name = _("HyperSpy")  # optional, specify default name if not a standard menu
     menu_before_id = "window_menu"  # optional, specify before menu_id if not a standard menu
+
+
+class RemoveBackgroundMenuItem(HyperSpyMenuItemBase):
+
     menu_item_name = _("Remove Background")  # menu item name
 
-    def menu_item_execute(self, document_window: API.DocumentWindow) -> None:
+    def menu_item_execute(self, window: API.DocumentWindow) -> None:
         #
         # WARNING: NION SWIFT VERSION DEPENDENT CODE. DO NOT USE AS AN EXAMPLE.
         #
@@ -64,7 +72,7 @@ class RemoveBackgroundMenuItem:
         # this code to work with new versions; however we will not update _your code_
         # based on this code. Please use the standard Nion Swift API instead.
         #
-        _document_window = document_window._document_window
+        _document_window = window._document_window
         display_specifier = _document_window.selected_display_specifier
         data_item = _document_window.document_model.make_data_item_with_computation("nion.hyperspy.background", [(display_specifier.data_item, None)], {"src": [None, None]})
         if data_item:
@@ -72,6 +80,24 @@ class RemoveBackgroundMenuItem:
             _document_window.display_data_item(new_display_specifier)
             return data_item
         return None
+
+
+class AlignZLPMenuItem(HyperSpyMenuItemBase):
+
+    menu_item_name = _("Align Zero Loss Peak")  # menu item name
+
+    def menu_item_execute(self, window: API.DocumentWindow) -> None:
+        data_item = window.target_data_item
+        if data_item:
+            xdata = data_item.xdata
+            if xdata.datum_dimension_count == 1:
+                signal = nion.hyperspy.xdata_to_signal(xdata, copy_data=True)
+                signal.set_signal_type('EELS')
+                signal.align_zero_loss_peak(print_stats=False)
+                xdata = nion.hyperspy.signal_to_xdata(signal)
+                data_item = window.library.create_data_item_from_data_and_metadata(xdata)
+                window.display_data_item(data_item)
+
 
 
 class HyperSpyExtension:
@@ -84,8 +110,8 @@ class HyperSpyExtension:
         api = api_broker.get_api(version="1", ui_version="1")
         # be sure to keep a reference or it will be closed immediately.
         self.__menu_item_ref = api.create_menu_item(RemoveBackgroundMenuItem())
+        self.__align_zlp_menu_item_ref = api.create_menu_item(AlignZLPMenuItem())
 
     def close(self):
-        # close will be called when the extension is unloaded. in turn, close any references so they get closed. this
-        # is not strictly necessary since the references will be deleted naturally when this object is deleted.
         self.__menu_item_ref.close()
+        self.__align_zlp_menu_item_ref.close()
